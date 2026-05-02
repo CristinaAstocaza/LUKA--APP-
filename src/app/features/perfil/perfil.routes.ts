7cfb2a08290decdf0fcab8c35ac158e1ab7c0c67
Cristina Astocaza <cristina01astocaza@gmail.com>
2026-05-02 10:35:28 -0500

    Sección Perfil del usuario-Con las rutas hijas

--- src/app/features/perfil/perfil.routes.ts
+++ src/app/features/perfil/perfil.routes.ts
@@ -1,18 +1,42 @@
-import { Routes } from "@angular/router";
-import { PerfilPage } from "./pages/perfil-page/perfil-page";
+import { Routes } from '@angular/router';
+import { PerfilLayout } from './perfil-layout/perfil-layout';
 
 export const PERFIL_ROUTES: Routes = [
-
-{
+  {
     path: '',
-    component: PerfilPage,
-    data: {
-        title: 'Perfil',
-        breadcrumbs: [
-            {label: 'Perfil', route: '/perfil'},
-            {label: 'Sección'}
-        ]
-    }
-}
-
-]
+    component: PerfilLayout,
+    children: [
+      { path: '', redirectTo: 'cliente', pathMatch: 'full' },
+      {
+        path: 'cliente',
+        loadComponent: () =>
+          import('./perfil-cliente/perfil-cliente')
+            .then(m => m.PerfilCliente)
+      },
+      {
+        path: 'financiero',
+        loadComponent: () =>
+          import('./perfil-financiero/perfil-financiero')
+            .then(m => m.PerfilFinanciero)
+      },
+      {
+        path: 'configuracion',
+        loadComponent: () =>
+          import('./configuracion/configuracion')
+            .then(m => m.Configuracion)
+      },
+      {
+        path: 'historial',
+        loadComponent: () =>
+          import('./historial/historial')
+            .then(m => m.Historial)
+      },
+      {
+        path: 'transacciones',
+        loadComponent: () =>
+          import('./transacciones/transacciones')
+            .then(m => m.Transacciones)
+      },
+    ]
+  }
+];
