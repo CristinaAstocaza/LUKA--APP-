a087fab75fdf1ec18a59287f1697cfff12f2c3e5
Cristina Astocaza <cristina01astocaza@gmail.com>
2026-05-26 18:15:42 -0500

    eliminacion de carpetas  temporal

--- estructura-backend/microservicio-mensajeria/src/main/java/com/mensajeria/aplicacion/dtos/respuestas/RespuestaGeneracion.java
+++
@@ -1,9 +0,0 @@
-package com.mensajeria.aplicacion.dtos.respuestas;
-
-import com.libreria.comun.enums.TipoVerificacion;
-
-public record RespuestaGeneracion(
-    boolean exito,
-    String mensaje,
-    TipoVerificacion tipo
-) {}
--- estructura-backend/microservicio-mensajeria/src/main/java/com/mensajeria/aplicacion/dtos/respuestas/RespuestaValidacion.java
+++
@@ -1,9 +0,0 @@
-package com.mensajeria.aplicacion.dtos.respuestas;
-
-/**
- * Respuesta estándar tras validar un código.
- */
-public record RespuestaValidacion(
-        boolean exito,
-        String mensaje) {
-}
